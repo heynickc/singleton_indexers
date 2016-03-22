c119e03b825074ac3b7b08c1aee2365b97c5a63d
nickc-ci <nchamberlain@cambridge-intl.com>
2016-03-22 17:00:28 -0400

    Piggybacking on old SingletonIndexer post

---
+++ IndexersDeepDive/JaggedArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersDeepDive {
+    public class AreaObjectCollection {
+        private Dictionary<int, Dictionary<int, List<AreaObject>>> _map = new Dictionary<int, Dictionary<int, List<AreaObject>>>();
+
+        public void Add(AreaId id, AreaObject obj) {
+            int x = id.X;
+            int y = id.Y;
+            if (!_map.ContainsKey(x)) {
+                _map.Add(x, new Dictionary<int, List<AreaObject>>());
+            }
+            if (!_map[x].ContainsKey(y)) {
+                _map[x].Add(y, new List<AreaObject>());
+            }
+            _map[x][y].Add(obj);
+        }
+    }
+    public class AreaId {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    public class AreaObject {
+        public string AreaId { get; set; }
+    }
+}
---
+++ IndexersDeepDive/SingletonIndexer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Xunit;
+
+namespace IndexersDeepDive {
+
+    public sealed class SingletonIndexer {
+
+        private static readonly Lazy<SingletonIndexer> lazy =
+            new Lazy<SingletonIndexer>(() => new SingletonIndexer());
+        private readonly ConcurrentDictionary<string, DataStoreClass> _properties;
+
+        public static SingletonIndexer Instance {
+            get { return lazy.Value; }
+        }
+        private SingletonIndexer() {
+            _properties = new ConcurrentDictionary<string, DataStoreClass>();
+        }
+        private void SetProperty(string name, DataStoreClass value) {
+            _properties.TryAdd(name, value);
+        }
+        private DataStoreClass GetProperty(string name) {
+            DataStoreClass value;
+            _properties.TryGetValue(name, out value);
+            return value;
+        }
+        public int GetCount() {
+            return _properties.Count;
+        }
+        public DataStoreClass this[string index] {
+            get { return GetProperty(index); }
+            set { SetProperty(index, value); }
+        }
+    }
+
+    public sealed class DataStoreClass {
+        public int Id { get; set; }
+        public string Attribute { get; set; }
+        public string SomeOtherAttribute { get; set; }
+    }
+
+    public class SingletonIndexerNonGeneric {
+        [Fact]
+        public void singleton_indexer_add_items() {
+            SingletonIndexer.Instance["Key1"] = new DataStoreClass();
+            Assert.IsType<DataStoreClass>(SingletonIndexer.Instance["Key1"]);
+        }
+    }
+}
